a0464f1119eebdca1d8600408ab95ccb5737a3a8
MarcAurico <hwsjyh2j52@privaterelay.appleid.com>
2026-08-05 19:58:36 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,22 @@
+# Pokémon Center Canada Tracker
+
+BASE_URL = "https://www.pokemoncenter.com/en-ca"
+
+# Discord
+DISCORD_WEBHOOK = "https://discord.com/api/webhooks/1534711095632203906/P7RISPJ3-dWz-Qe2DAL2pOkjPlW7NqSIq5nGlbUDg9OQP2607Mi6aL6tQW-MUJAb2Zib"
+
+# Mots-clés importants
+KEYWORDS = [
+    "151",
+    "Prismatic",
+    "Black Bolt",
+    "White Flare",
+    "Team Rocket",
+    "Elite Trainer Box",
+    "ETB",
+    "Ultra Premium Collection",
+    "Booster Bundle"
+]
+
+# Temps d'attente entre les vérifications
+CHECK_INTERVAL = 300
